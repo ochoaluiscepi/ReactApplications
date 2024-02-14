bdd5ae1492d92b8d3ff45e4db7fda32d105c9b57
ochoaluis_cepi <ochoaluis_cepi@hotmail.com>
2024-02-14 13:51:22 -0600

    Practice Scripts

---
+++ PracticeScripts/PracticeScripts.Tests/Arcade/EdgeOfTheOceanTest.cs
@@ -0,0 +1,25 @@
+﻿using PracticeScripts.Arcade;
+
+namespace PracticeScripts.Tests.Arcade
+{
+    public class EdgeOfTheOceanTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CenturyFromYearTest()
+        {
+            //Arrange
+
+            int[] arr = new int[]{5, 6, -4, 2, 3, 2, -23};
+            EdgeOfTheOcean thej = new EdgeOfTheOcean();
+            //Act
+            int result = thej.AdjacentElementsProduct(arr);
+            //Assert
+            Assert.IsTrue(result == 30);
+        }
+    }
+}
---
+++ PracticeScripts/PracticeScripts/Arcade/EdgeOfTheOcean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeScripts.Arcade
+{
+    public class EdgeOfTheOcean
+    {
+        public int AdjacentElementsProduct(int[] inputArray)
+        {
+            return inputArray.Select((i, j) => j > 0 ? i * inputArray[j - 1] : int.MinValue).Max();
+        }
+    }
+}
